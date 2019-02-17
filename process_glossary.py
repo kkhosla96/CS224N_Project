fe79f7d4eb4fcc0205f47342d6f82124e299d407
Nicholas Bowman <nbowman@stanford.edu>
2019-02-17 00:24:33 -0800

    nick tries to do something without brutally destroying everything

---
+++ process_glossary.py
@@ -0,0 +1,10 @@
+import csv
+
+data = "data/life_glossary.csv"
+outputFile = "data/life_gold.txt"
+
+with open(data) as data_csv:
+	glossary_reader = csv.reader(data_csv)
+	for row in glossary_reader:
+		print row[0].lower()
+
