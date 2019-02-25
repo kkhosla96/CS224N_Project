b62cd375dad732fd001c0b312eced8208e83c3a0
Kush Khosla <kkhosla@stanford.edu>
2019-02-25 00:39:29 -0800

    more robust cnn. implements the cnn from the original paper.

--- CNN.py
+++ CNN.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -6,39 +7,54 @@ class CNN(nn.Module):
 	A simple CNN network to determine if a term is a glossary term.
 	"""
 
-	def __init__(self, height, word_embed_size, kernel_size=3, max_pool_kernel=2):
+	def __init__(self, vocab, length_of_term, word_embed_size, out_channels, kernel_sizes=None):
 		'''
 		Need to supply the hyper-parameters that define the CNN network architecture.
-		Right now, this will be a simple CNN.
+		
+		@param vocab (Vocab): a class to help get embeddings from words
+		@param length_of_term (int): corresponds to l in the original paper, how many words the input term is. 
+		It's possible will need to pad input to make them the correct size.
+		@param word_embed_size (int): the length of the embedding for a word. corresponds to d in the original paper.
+		@param out_channels (int): how many filters to have for the convolution of each size. corresponds to n in the original paper.
+		@param kernel_size (List[int]): list of the sizes for the convolutions (for example, look at bigrams, trigrams, quadgrams etc.).
+		the length of this list corresponds to r in the original paper.
 		'''
 		super(CNN, self).__init__()
+		self.vocab = vocab
+		self.length_of_term = length_of_term
 		self.word_embed_size = word_embed_size
-		self.kernel_size = kernel_size
-		self.height = height
-
+		self.out_channels = out_channels
 		self.in_channels = 1
-		self.out_channels = 3
-		self.conv = nn.Conv2d(self.in_channels, self.out_channels, self.kernel_size)
-		conv_H_out = self.height - kernel_size + 1
-		conv_W_out = self.word_embed_size - kernel_size + 1
-		conv_out_size = (-1, self.out_channels, conv_H_out, conv_W_out)
-		self.max_pool = nn.MaxPool2d(max_pool_kernel)
-		max_pool_H_out = int((conv_H_out - max_pool_kernel) / (max_pool_kernel) + 1)
-		max_pool_W_out = int((conv_W_out - max_pool_kernel) / (max_pool_kernel) + 1)
-		max_pool_size = (-1, self.out_channels, max_pool_H_out, max_pool_W_out)
-		self.linear = nn.Linear(self.out_channels * max_pool_H_out * max_pool_W_out, 1)
+		if kernel_sizes is None:
+			self.kernel_sizes = [s for s in range(2, length_of_term + 1)]
+		else:
+			self.kernel_sizes = kernel_sizes
+		
+		self.convs = [nn.Conv2d(self.in_channels, self.out_channels, (kernel_size, self.word_embed_size)) for kernel_size in self.kernel_sizes]
+		self.linear = nn.Linear(len(self.kernel_size) * self.out_channels, 1)
 
+	'''	
+	@param terms (List[List[str]]): a list of terms to evaluate, each of which are a list of strings. 
+	@returns probabilities (Tensor): a tensor of shape (len(terms),) that represents the probability each term is a key-phrase.
 	'''
-	mat has size (batch_size, 1, height, embed_size)
-	'''
-	def forward(self, mat):
-		conv = self.conv(mat)
-		rel = F.relu(conv)
-		pooled = self.max_pool(rel)
-		flat = pooled.view(pooled.size()[0], -1)
-		out = self.linear(flat)
-		prob = F.sigmoid(out)
-		return prob.squeeze()
+	def forward(self, terms):
+		# TODO: figure out how to get from terms to embeddings of shape (batch_size, self.in_channels, self.length_of_term, self.word_embed_size)
+		# for now, assume that there is a variable called embeddings that is just that.
+
+		# each thing in feature_maps is of size (batch_size, self.out_channels, h_out, 1), where the last two terms are computed
+		# using the formula here: https://pytorch.org/docs/stable/nn.html#torch.nn.Conv2d.
+		feature_maps = [conv(embeddings) for conv in self.convs]
+		# we squeeze so each thing is of size (batch_size, self.out_channels, h_out), as it looks in the paper
+		squeezed = [torch.squeeze(fm, dim=3) for fm in feature_maps]
+		# we take the maximum for each fm, each element in maxes has shape (batch_size, self.out_channels)
+		maxes = [torch.max(fm, dim=2) for fm in squeezed]
+		# concat all of them to send to the final layer, should have size (batch_size, len(self.kernel_size) * self.out_channels)
+		cat = torch.cat(maxes, dim=1)
+		# multiply by linear layer and put through sigmoid. vals, since we squeeze, should have shape (batch_size) 
+		vals = torch.squeeze(self.linear(cat))
+		# finally, we pass it through a sigmoid to get a probability. prob should also have shape (batch_size)
+		probs = torch.sigmoid(self.linear(vals))
+		return probs
 
 	def train(self, X_train, y_train):
 		self.X_train = X_train
