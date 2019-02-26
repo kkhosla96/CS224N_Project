3f71b81d7b1ad0f26f616f2b9a3a48b8a70639e3
Kush Khosla <kkhosla@stanford.edu>
2019-02-25 22:52:12 -0800

    fixes devices and stuff so cnn now actually runs

--- CNN.py
+++ CNN.py
@@ -30,7 +30,7 @@ class CNN(nn.Module):
 			self.kernel_sizes = kernel_sizes
 		
 		self.convs = [nn.Conv1d(self.in_channels, self.out_channels, kernel_size) for kernel_size in self.kernel_sizes]
-		self.linear = nn.Linear(len(self.kernel_size) * self.out_channels, 1)
+		self.linear = nn.Linear(len(self.kernel_sizes) * self.out_channels, 1)
 		self.embeddings = nn.Embedding.from_pretrained(embeddings)
 
 	def forward(self, terms):
@@ -40,7 +40,7 @@ class CNN(nn.Module):
 		'''
 
 		# indices is of size (batch_size, self.max_term_length)
-		indices = self.vocab.to_input_tensor(terms, self.device)
+		indices = self.vocab.to_input_tensor(terms)
 		# embeddings is of size (batch_size, self.max_term_length, self.word_embed_size)
 		embeddings = self.embeddings(indices)
 		# we transpose the two dimensions because the convolution networks expect an input
@@ -50,7 +50,7 @@ class CNN(nn.Module):
 		# using the formula here: https://pytorch.org/docs/stable/nn.html#torch.nn.Conv1d.
 		feature_maps = [conv(embeddings) for conv in self.convs]
 		# we take the maximum for each fm, each element in maxes has shape (batch_size, self.out_channels)
-		maxes = [torch.max(fm, dim=2) for fm in feature_maps]
+		maxes = [torch.max(fm, dim=2)[0] for fm in feature_maps]
 		# concat all of them to send to the final layer, should have size (batch_size, len(self.kernel_size) * self.out_channels)
 		cat = torch.cat(maxes, dim=1)
 		# multiply by linear layer and put through sigmoid. vals, since we squeeze, should have shape (batch_size) 
