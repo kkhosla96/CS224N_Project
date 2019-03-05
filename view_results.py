5fbb8c2de4a8d1a2a4d1d2a93980d54361b517b4
Kush Khosla <kkhosla@stanford.edu>
2019-03-05 14:26:26 -0800

    precision and recall now take into account seed set

--- view_results.py
+++ view_results.py
@@ -6,11 +6,12 @@ open a Python shell in the same directory as this file, and run from view_result
 then, you can access these variables.
 '''
 
-labeled_file = "./data/output/single_cnn_labeled_data_first_try.pkl"
-labels_file = "./data/output/single_cnn_labels_first_try.pkl"
+labeled_file = "./data/output/single_cnn_labeled_data_third_try.pkl"
+labels_file = "./data/output/single_cnn_labels_third_try.pkl"
+seed_file = "./data/seed_sets/openstax_biology_seed_set.txt"
 gold_file = "./data/gold/openstax_biology_gold.pkl"
 
-precision, recall = utils.calculate_precision_and_recall(labeled_file, labels_file, gold_file)
+precision, recall = utils.calculate_precision_and_recall(labeled_file, labels_file, seed_file, gold_file)
 
 labeled, labels = utils.get_labeled_and_labels(labeled_file, labels_file)
 golds = utils.get_gold_terms(gold_file)
