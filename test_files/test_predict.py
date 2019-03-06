4c0ce19d76b8929bfd35148373c84db1bab4c336
Kush Khosla <kkhosla@stanford.edu>
2019-03-06 02:52:56 -0800

    created a general experiment method

--- test_files/test_predict.py
+++ test_files/test_predict.py
@@ -13,7 +13,7 @@ number_words = 1000
 embeddings = torch.nn.Embedding(number_words, embed_size)
 print(embeddings.weight.type())
 vocab = Vocab("sample_vocabulary.txt", 3)
-cnn = CNN(vocab, 3, embeddings)
+cnn = CNN(vocab, embeddings)
 terms = [["this", "vocabulary"], ["it", "bitch"], ["hello"]]
 f = cnn.forward(terms)
 print(f)
