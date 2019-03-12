81e941741274c6ea0cabdf2873541d933c79aa1c
Kush Khosla <kkhosla@stanford.edu>
2019-03-11 19:03:19 -0700

    test file to overtrain the lstm

---
+++ test_files/test_overtrain_lstm.py
@@ -0,0 +1,30 @@
+import sys
+import torch
+import matplotlib.pyplot as plt
+
+sys.path.append("..")
+
+from LSTM import LSTM
+from WordVectorParser import WordVectorParser
+
+
+test_file = "fasttext_pretrained_candidates.vec"
+
+wvp = WordVectorParser(test_file)
+vocab = wvp.get_vocab()
+embedding_layer = wvp.get_embedding_layer()
+
+# train_X = [["carbohydrate"], ["calories"], ["cell", "cycle"], ["primary", "structure"], ["structural", "isomers"], ["textbooks"], ["many", "tires"], ["many"], ["groups"], ["surround"]]
+# train_y = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+# train_X = [["carbohydrate"], ["cell", "cycle"], ["primary", "structure"], ["textbooks"], ["groups"]]
+# train_y = [1, 1, 1, 0, 0]
+train_X = [["carbohydrate"], ["calories"], ["cell", "cycle"], ["many"], ["textbooks"]]
+train_y = [1, 1, 1, 0, 0]
+
+lstm = LSTM(vocab=vocab, embeddings=embedding_layer)
+losses = lstm.train_on_data(train_X, train_y, num_epochs=200, lr=.01, betas=(.9, .999), batch_size=2)
+print(lstm.forward(train_X))
+plt.plot(losses)
+plt.show()
+
+
