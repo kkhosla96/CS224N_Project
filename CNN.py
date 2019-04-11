9fd2eeb4db9f54c078db1d9ee19970c1357ffe46
Kush Khosla <kkhosla@stanford.edu>
2019-04-11 10:40:48 -0700

    debugging

--- CNN.py
+++ CNN.py
@@ -36,7 +36,8 @@ class ListModule(nn.Module):
 	def __len__(self):
 		return len(self._modules)
 
-
+for i in range(10:
+	print("hello")
 
 class CNN(nn.Module):
 	"""
@@ -199,19 +200,32 @@ class DeepCNN(nn.Module):
 		if self.gpu:
 			indices = indices.cuda()
 		embeddings = self.embedding_layer(indices)
+		print(embeddings.size())
 		embeddings = self.dimensionality_reduction(embeddings)
+		print(embeddings.size())
 		embeddings = torch.transpose(embeddings, 1, 2)
+		print(embeddings.size())
 		feature_maps = [self.grams[idx].forward(embeddings) for idx in range(len(self.grams))]
+		print([fm.size() for fm in feature_maps])
 		cat = torch.cat(feature_maps, dim=2)
+		print(cat.size())
 		cat = torch.transpose(cat, 1, 2)
+		print(cat.size())
 		maxd = self.max1(cat)
+		print(maxd.size())
 		relud = self.relu(maxd)
+		print(relud.size())
 		unsqueezed = torch.unsqueeze(relud, dim=1)
+		print(unsqueezed.size())
 		convd = self.conv(unsqueezed)
+		print(convd.size())
 		maxd2 = self.max2(convd)
+		print(maxd2.size())
 		flat = maxd2.view(maxd2.size()[0], -1)
+		print(flat.size())
 		dropped = self.dropout(flat)
 		scores = self.linear(dropped)
+		print(scores.size())
 		scores = scores.squeeze(dim=1)
 		probs = torch.sigmoid(scores) 
 		return probs
--- test_files/test_overtrain_fullyconnected.py
+++ test_files/test_overtrain_fullyconnected.py
@@ -7,20 +7,21 @@ sys.path.append("..")
 from FullyConnected import FullyConnected
 from WordVectorParser import WordVectorParser
 
+test_file = "../data/vectors/openstax_biology/averagebertvectors.vec"
 
-test_file = "fasttext_pretrained_candidates.vec"
-
-wvp = WordVectorParser(test_file)
+wvp = WordVectorParser(test_file, word_vector_length=768)
 vocab = wvp.get_vocab()
 embedding_layer = wvp.get_embedding_layer()
 
-train_X = [["carbohydrate"], ["calories"], ["cell", "cycle"], ["primary", "structure"], ["structural", "isomers"], ["textbooks"], ["many", "tires"], ["many"], ["groups"], ["surround"]]
-train_y = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+# train_X = [["carbohydrate"], ["calories"], ["cell", "cycle"], ["primary", "structure"], ["structural", "isomers"], ["textbooks"], ["many", "tires"], ["many"], ["groups"], ["surround"]]
+# train_y = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
 # train_X = [["carbohydrate"], ["cell", "cycle"], ["primary", "structure"], ["textbooks"], ["groups"]]
 # train_y = [1, 1, 1, 0, 0]
+train_X = [["electrolyte"]]
+train_y = [1]
 
 net = FullyConnected(vocab, embedding_layer)
-losses = net.train_on_data(train_X, train_y, num_epochs=200, lr=.01, momentum=0.99)
+losses = net.train_on_data(train_X, train_y, num_epochs=1, lr=.01, momentum=0.99)
 print(net.forward(train_X))
 plt.plot(losses)
 plt.show()
