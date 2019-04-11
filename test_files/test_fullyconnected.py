47e2b7c4a9702d9adc36315b4f91fb0dfc40f321
Kush Khosla <kkhosla@stanford.edu>
2019-04-11 10:35:25 -0700

    Update test_fullyconnected.py

--- test_files/test_fullyconnected.py
+++ test_files/test_fullyconnected.py
@@ -14,7 +14,7 @@ embeddings = torch.nn.Embedding(number_words, embed_size)
 print(embeddings.weight.type())
 vocab = Vocab("sample_vocabulary.txt", 3)
 net = FullyConnected(vocab, embeddings)
-terms = [["this", "vocabulary"], ["it", "bitch"], ["hello"]]
+terms = [["this", "vocabulary"], ["it", "friend"], ["hello"]]
 f = net.forward(terms)
 print(f)
 print(f.size())
